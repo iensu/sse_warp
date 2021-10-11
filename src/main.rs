de71ae71616a655d1505771f1f074767b3430fc5
Jens Östlund <jostlund@gmail.com>
2021-10-11 16:08:20 +0200

    Remove unused files

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use warp::Filter;
-
 mod api;
 mod util;
 
@@ -14,12 +12,10 @@ mod util;
 async fn main() {
     pretty_env_logger::init();
 
-    let chat_clients = Arc::new(Mutex::new(HashMap::new()));
     let game_clients = Arc::new(Mutex::new(HashMap::new()));
     let games: Games = Arc::new(Mutex::new(HashMap::new()));
 
-    let routes =
-        api::chat::chat_routes(chat_clients).or(api::games::game_routes(games, game_clients));
+    let routes = api::games::game_routes(games, game_clients);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
